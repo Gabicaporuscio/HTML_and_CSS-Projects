324cfef11039867b9c56c10c7ef83da849c89405
Gabicaporuscio <91995894+Gabicaporuscio@users.noreply.github.com>
2021-10-14 16:24:41 +0100

    assigment

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,12 @@
+<! DOCTYPE html>
+<html>
+<script>
+  function My_First_Function() {
+    var String = "kiss me, I'm Irish!";
+    var result = String.frontcolor("green");
+    document.getElementById("Irish").innerHTML = result;
+    }
+    alert("Hello!");
+</script>
+
+</html>
